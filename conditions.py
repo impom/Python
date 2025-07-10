2face33f7d40be1bcd0d411a75f3e80e5904fb39
impom <117834750+impom@users.noreply.github.com>
2025-07-10 11:43:36 +0530

    F string literal with if else statement

---
+++ conditions.py
@@ -0,0 +1,7 @@
+print("Enter two numbers : ")
+a = int(input("First number: "))
+b = int(input("Second number: "))
+if a>b:
+    print(f"{a} is greater than {b}") #Obviously i know what a formatted string literal is , i am learning python since i was 16 , now i am 18
+else:
+    print(f"{b} is greater than {a}")
